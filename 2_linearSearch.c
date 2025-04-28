2386c34690afa63810bd361549339cf2789e804a
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-28 22:09:14 +0530

    added

--- 2_linearSearch.c
+++ 2_linearSearch.c
@@ -18,3 +18,22 @@ int linearSearch(int *a, int n, int k)
     }
     return -1;
 }
+void tester()
+{
+    int *arr, n, key, ch, ans;
+    printf("\nEnter array size: ");
+    scanf("%d", &n);
+    arr = (int *)malloc(n * sizeof(int));
+    printf("\nEnter array elements: ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", (arr + i));
+    }
+    printf("\nEnter key to be searched : ");
+    scanf("%d", &key);
+    ans = linearSearch(arr, n, key);
+    if (ans == -1)
+        printf("\nKey not found! %d\n", ans);
+    else
+        printf("\nKey found at %d index\n", ans);
+}
