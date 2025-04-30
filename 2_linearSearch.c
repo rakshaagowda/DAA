e2263f0c8e3d96aaebcdb9b4f2691dd61662905d
RAKSHA B R <rakshagowdah264@gmail.com>
2025-04-30 21:37:32 +0530

    added

--- 2_linearSearch.c
+++ 2_linearSearch.c
@@ -38,3 +38,36 @@ void tester()
     else
         printf("\nKey found at %d index\n", ans);
 }
+void plotter()
+{
+    srand(time(NULL));
+    int *arr;
+    int n, key, r;
+    FILE *f1, *f2, *f3;
+    f1 = fopen("linearbest.txt", "w");
+    f2 = fopen("linearavg.txt", "w");
+    f3 = fopen("linearworst.txt", "w");
+    n = 2;
+    while (n <= 1024)
+    {
+        arr = (int *)malloc(n * sizeof(int));
+        for (int i = 0; i < n; i++)
+            *(arr + i) = 1;
+        r = linearSearch(arr, n, 1);
+        fprintf(f1, "%d\t%d\n", n, count);
+        for (int i = 0; i < n; i++)
+            *(arr + i) = rand() % n;
+        key = rand() % n;
+        r = linearSearch(arr, n, key);
+        fprintf(f2, "%d\t%d\n", n, count);
+        for (int i = 0; i < n; i++)
+            *(arr + i) = 0;
+        r = linearSearch(arr, n, 1);
+        fprintf(f3, "%d\t%d\n", n, count);
+        n = n * 2;
+        free(arr);
+    }
+    fclose(f1);
+    fclose(f2);
+    fclose(f3);
+}
